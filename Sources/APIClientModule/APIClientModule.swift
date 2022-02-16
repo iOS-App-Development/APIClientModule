d3ffafb564ffd11ae0cb121fc97afe75b7488355
Muzammil Peer <muzammilpeer987@gmail.com>
2022-02-17 01:08:52 +0400

    main class added

--- Sources/APIClientModule/APIClientModule.swift
+++ Sources/APIClientModule/APIClientModule.swift
@@ -1,6 +1,18 @@
+import Foundation
+import APIClient
+
 public struct APIClientModule {
     public private(set) var text = "Hello, World!"
 
     public init() {
+    }
+}
+
+extension APIClientModule:APIClientModuleAPI
+{
+    func load() {
+    }
+    
+    func unload() {
     }
 }
---
+++ Sources/APIClientModule/APIClientModuleAPI.swift
@@ -0,0 +1,13 @@
+//
+//  APIClientModuleAPI.swift
+//  
+//
+//  Created by Muzammil Peer on 17/02/2022.
+//
+
+import Foundation
+
+protocol APIClientModuleAPI {
+    func load()
+    func unload()
+}
